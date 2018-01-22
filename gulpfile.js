ce171602124178c322e7432455f33ecce7d19db0
Ilya Kushlianski <kushliansky@gmail.com>
2018-01-22 03:23:27 +0300

    Working conditional resizing and minification of images

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
 var gulp       = require('gulp'), // Подключаем Gulp
 	rename       = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
 	del          = require('del'), // Подключаем библиотеку для удаления файлов и папок
-	imagemin     = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
-	pngquant     = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
 	fs 					 = require('fs'),
 	path				 = require('path'),
 	using 			 = require('gulp-using'),
 	vinylPaths 	 = require('vinyl-paths'),
 	cache        = require('gulp-cache'),
-	imageResize  = require('gulp-image-resize'),
+	jimp         = require("gulp-jimp-resize"),
 	image        = require('gulp-image'),
 	watch 		   = require('gulp-watch'),
 	filelog 		 = require('gulp-filelog');
@@ -25,36 +23,50 @@ var gulp       = require('gulp'), // Подключаем Gulp
 // 		.pipe(gulp.dest('./')); // Выгружаем на продакшен
 // });
 
-gulp.task('ctv', ['optimize'], function(){
+gulp.task('ctv', /*['optimize', 'deleteImg'],*/ function(){
 	return watch('images_to_optimize/*.{png,jpg,jpeg,JPG}', function() {
-		setTimeout(function(){
-			gulp.start('optimize');
-		}, 2000);
-  });
-	// gulp.watch('images_to_optimize/*.jpg', ['moveFiles']);
+        fs.readdir('images_to_optimize/', function(err, files) {
+            if (err) {
+               // some sort of error
+            } else {
+               if (files.length) {
+                //    setTimeout(function(){
+           			   gulp.start(['deleteImg']);
+          //  		   }, 1000);
+               }
+            }
+        });
+    });
 });
 
-gulp.task('optimize', ['moveFiles'], function () {
-  return gulp.src('./optimized/**/*.{png,jpg,jpeg,JPG}')
-		.pipe(image())
-		.pipe(gulp.dest(function(file) {
-    	return file.base;
-  	}));
+gulp.task('deleteImg', ['optimize'], function() {
+	del.sync(['images_to_optimize/**', '!images_to_optimize']);
 });
 
-gulp.task('moveFiles', function () {
+gulp.task('optimize', function () {
   return gulp.src('./images_to_optimize/**/*.{png,jpg,jpeg,JPG}')
-	.pipe(vinylPaths(function (paths) {
-		let oldPath = path.parse(paths).dir;
-		let myfilename = path.parse(paths).base;
-		let newPath = path.normalize("D:\\temp\\ctvimages\\optimized") + path.normalize("\\") + myfilename;
-		fs.rename(oldPath + path.normalize("\\") + myfilename, newPath, function(){
-			console.log('files moved');
-		});
-		return Promise.resolve();
-
-	}));
+        .pipe(jimp({
+            sizes: [
+                {"suffix": "md", "width": 800, "upscale": false}
+            ],
+        }))
+        .pipe(image())
+		.pipe(gulp.dest('./optimized'));
 });
+
+// gulp.task('moveFiles', function () {
+//   return gulp.src('./images_to_optimize/**/*.{png,jpg,jpeg,JPG}')
+// 	.pipe(vinylPaths(function (paths) {
+// 		let oldPath = path.parse(paths).dir;
+// 		let myfilename = path.parse(paths).base;
+// 		let newPath = path.normalize("D:\\temp\\ctvimages\\optimized") + path.normalize("\\") + myfilename;
+// 		fs.rename(oldPath + path.normalize("\\") + myfilename, newPath, function(){
+// 			console.log('files moved');
+// 		});
+// 		return Promise.resolve();
+//
+// 	}));
+// });
 
 gulp.task('img-wp', function() {
 	return gulp.src('./images_to_optimize/**/*.{png,jpg,jpeg,JPG}') // Берем все изображения
